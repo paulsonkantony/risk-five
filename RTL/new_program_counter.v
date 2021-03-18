fd89d68e3352735c1dfecfc610e32c75e1f8e4aa
Nikshith Narayan Ramesh <nikky.narayan854@gmail.com>
2021-03-18 12:06:38 +0530

    Update new_program_counter.v

--- RTL/new_program_counter.v
+++ RTL/new_program_counter.v
@@ -2,11 +2,13 @@ module new_program_counter(D,reset,Q);
 	input [31:0] D; // Data input 
 	input reset; // asynchronous active low reset 
 	output reg [31:0] Q; // output Q 
-	always @(negedge reset or D) 
+	always @(negedge reset) 
 	begin
 		if (!reset) 
 			Q<=32'b0;
-		else 
-			Q<=D;
+	end
+	always @(D) 
+	begin
+		Q<=D;
 	end
 endmodule 

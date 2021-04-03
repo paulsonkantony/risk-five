90017a24562f432a33e953e9d0bb65392f50003b
Nikshith Narayan Ramesh <nikky.narayan854@gmail.com>
2021-04-03 20:39:17 +0530

    Update rvb_xperm.v

--- RTL/Bitmanip/rvb_xperm.v
+++ RTL/Bitmanip/rvb_xperm.v
@@ -22,13 +22,14 @@ module rvb_xperm (xperm_valid, op_xperm_n, op_xperm_b,rs1,rs2,res);
 		if(xperm_valid)
 		begin
 		res = 0;
-		for (i = 0; i < 32; i = i + sz) 
+		for (i = 0; i < 32; i = i) 
 		    begin
 		        pos = ((rs2 >> i) & mask) << sz_log2;
 		        if (pos < 32) 
 		            begin 
 		                res <= res | ((rs1 >> pos) & mask) << i;
-		            end    
+		            end 
+			    i = i + sz;
 		    end
 		end
 	end

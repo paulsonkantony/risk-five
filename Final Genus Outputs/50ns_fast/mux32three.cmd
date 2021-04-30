d043cb06739b5e7da07ceeaa2fce635e0f981ae9
panda <panda>
2021-04-30 21:43:56 +0530

    keeping all the outputs in the folder Final Genus Outputs.

---
+++ Final Genus Outputs/50ns_fast/mux32three.cmd
@@ -0,0 +1,14 @@
+# Cadence Genus(TM) Synthesis Solution, Version 17.21-s010_1, built Feb  7 2018
+
+# Date: Thu Apr 29 19:41:16 2021
+# Host: cad54 (x86_64 w/Linux 2.6.32-71.el6.x86_64) (2cores*4cpus*1physical cpu*Intel(R) Core(TM) i3-2120 CPU @ 3.30GHz 3072KB)
+# OS:   Red Hat Enterprise Linux Server release 6.0 (Santiago)
+
+set_db library fast.lib
+read_hdl mux32three.v
+elaborate
+synthesize -to_mapped -effort medium
+report area
+report timing
+report power
+exit
